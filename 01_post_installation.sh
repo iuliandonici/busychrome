a252f49ada28a82a1826b46ffa4037f9b97c4752
iuliandonici <iuliandonici@gmail.com>
2022-05-30 15:27:25 +0200

    Is this working?

---
+++ 01_post_installation.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Things to do after an MX Linux installation
+
+# Update and upgrade
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get autoremove -y --purge
+sudo apt-get update
+
+# Install VS Codium
+# - Add the GPG key of the repository:
+wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg \
+    | gpg --dearmor \
+    | sudo dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg
+# - Add the repository
+echo 'deb [ signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg ] https://download.vscodium.com/debs vscodium main' \
+    | sudo tee /etc/apt/sources.list.d/vscodium.list
+# - Finally, install VS Codium
+sudo apt update && sudo apt install codium -y
+
+
