4ff5660b7e341141c52627329a96de16ff226bb4
Iulian Donici <iuliandonici@gmail.com>
2022-05-31 20:08:34 +0200

    Update x01_make_bootable_usb.sh

--- x01_make_bootable_usb.sh
+++ x01_make_bootable_usb.sh
@@ -3,10 +3,10 @@
 # and install a different BIOS
 
 # - Download MX using a shortcut domain like 2bn.ro
-wget 2bn.ro/mxiso
+curl -L0 2bn.ro/mxiso --output mx.iso
 # - Then, put the ISO to the first /dev/sda that you got,
 # your 1st USB, basically.
-sudo dd if=mxiso bs=4M of=/dev/sda oflag=sync status=progress
+dd if=mx.iso bs=4M of=/dev/sda oflag=sync status=progress
 
 # Get the required file for SEABios
  cd; curl -LO https://github.com/iuliandonici/busychrome/blob/master/firmware-util.sh && sudo bash firmware-util.sh
