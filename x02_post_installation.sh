32c19c56dcf8444abad99fb1ec0c33d9b5f560a1
iuliandonici <iuliandonici@gmail.com>
2022-06-04 09:59:22 +0200

    Removed git config

--- x02_post_installation.sh
+++ x02_post_installation.sh
@@ -40,15 +40,7 @@ sudo rm -rf ~/.ssh/id_rsa.pub
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCjDQNLdJA6smnaFWsA/emI2U56klErgKaPvX2rq5HbE9oogl/3u4qbZts6vdmXJel3oxidGLqWIV9V0LIYW+QC5JFHCdzt//2pp0k2QWE2RaL6xReR1PDkrSGikcKax2Xp73Ej1DcdQpwGhhLfAGs4JNvB5/D/IVK2Lcssg5+qajGouaBC6QZ3xDmZZaYyOAmLb+wpNmqyaW/YwreAXZ8c77rdC+g1MLnWOnJoNcAe0//YQ/b3MIlkuGiRaviT/C8i2Y2wBCZKjU3oMiAx+q0uG3NEk1YeyZtMtywHR+iWlpEMJohJBcxhc+Ne6AJJzI4dZHoxLpJReGg8Sv+7xCX9eDLCDTodIU/SVXrBMoedcJn9NcJKLY3ABJIOW6vnLRqTuBiw5Vw1t3U3o07r2/ggltdxoxHcejdvz/ORmX/VbbsGGIHhyct8EG/kYUhuYv/cS1ckDyN8asYfcP8qy87Qox1zgoAWcLEJocubhGVs+O1oNGPHxQLj9KMcZMqf/wk= busyneo@busychrome" >id_rsa.pub
 cp -r id_rsa.pub ~/.ssh/
 # Initiate a git repository
-git init
-# - Clone the repositories: busychrome
-git clone https://github.com/iuliandonici/busychrome.git
-# - Clone the repositories: neotestertheme
-git clone https://github.com/iuliandonici/neotestertheme.git
-# Add remote "busychrome" repository
-git remote add busychrome git@github.com:iuliandonici/busychrome.git
-# - Add remote "neotestertheme" repository
-git remote add neotestertheme git@github.com:iuliandonici/neotestertheme.git
+
 
 # Install system components
 sudo apt update  && sudo apt install -y curl gnupg apt-transport-https
